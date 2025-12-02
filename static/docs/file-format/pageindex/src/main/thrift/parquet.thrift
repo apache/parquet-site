79082ec5e953e92d34ce800bcf4abcb39bd30553
Micah Kornfield <micah.kornfield@databricks.com>
2025-12-02 22:38:41 +0000

    Use docs from parquet-format when appropriate

---
+++ static/docs/file-format/pageindex/src/main/thrift/parquet.thrift
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Redirecting to parquet.thrift on GitHub</title>
+    <meta http-equiv="refresh" content="0; url=https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift">
+    <link rel="canonical" href="https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift">
+</head>
+<body>
+    <p>Redirecting to <a href="https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift">parquet.thrift on GitHub</a>...</p>
+    <script>
+        window.location.href = "https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift";
+    </script>
+</body>
+</html>
