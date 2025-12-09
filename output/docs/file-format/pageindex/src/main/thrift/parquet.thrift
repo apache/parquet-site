de0a766d9993456456cbead6e9222a0f911899c5
emkornfield <emkornfield@users.noreply.github.com>
2025-12-09 19:39:22 +0000

    deploy: b588d89f68fc261905f80474a4175036a005fec7

---
+++ output/docs/file-format/pageindex/src/main/thrift/parquet.thrift
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!-- Local redirect to latest parquet-thrift file -->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Redirecting to parquet.thrift on GitHub</title>
+    <meta http-equiv="refresh" content="0; url=https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift">
+    <link rel="canonical" href="https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift">
+</head>
+<body>
+    <p>Redirecting to <a href="https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift">parquet.thrift on GitHub</a>...</p>
+    <script>
+        window.location.href = "https://github.com/apache/parquet-format/blob/master/src/main/thrift/parquet.thrift";
+    </script>
+</body>
+</html>
